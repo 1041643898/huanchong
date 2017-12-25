3ae2b00a90af37a8925d3b5e1b64507bf3afb9f8
Administrator <1363239411@qq.com>
2017-12-25 09:09:46 +0800

    孟浩东第八次提交

--- app/src/main/java/com/example/a666/petapp/HomePageActivity.java
+++ app/src/main/java/com/example/a666/petapp/HomePageActivity.java
@@ -519,7 +519,6 @@ public class HomePageActivity extends BaseActivity implements View.OnClickListen
                 //搜索城市
                 break;
 
-
             case R.id.image_Orientation:
                 //定位
                 // startActivity(new Intent(HomePageActivity.this, LocationActivity.class));
